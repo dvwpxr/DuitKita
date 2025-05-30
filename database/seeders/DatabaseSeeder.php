bc22c3b6069c01114bf76647e1cc24711be515a5
dvwpxr <davacepot@gmail.com>
2025-05-31 05:17:33 +0700

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,19 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create(); // You can keep this if you want other random users
-
-        User::factory()->create([
-            'name' => 'dava',
-            'email' => 'dava@example.com',
-            'password' => Hash::make('password123'), // Set a secure password
-        ]);
-
-        User::factory()->create([
-            'name' => 'albella',
-            'email' => 'albella@example.com',
-            'password' => Hash::make('password123'), // Set a secure password
-        ]);
 
         // Optionally, create a default Test User if needed
         // User::factory()->create([
